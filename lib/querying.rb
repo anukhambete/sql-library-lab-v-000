ed9a52a1f6871e02510912a2b1c8565fe83efb6e
Anu Khambete <anukhambete@gmail.com>
2018-06-11 21:39:09 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -32,6 +32,6 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, characters.series_id FROM characters
+  "SELECT * FROM characters
    JOIN books ON books.series_id = characters.series_id;"
 end
