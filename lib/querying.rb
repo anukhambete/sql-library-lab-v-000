02f45e7915b32d9a4a5875dcc46f85ea26003327
Anu Khambete <anukhambete@gmail.com>
2018-06-11 21:46:45 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -33,5 +33,5 @@ end
 
 def select_character_names_and_number_of_books_they_are_in
   "SELECT * FROM character_books
-   JOIN characters ON character.id = character_books.character_id;"
+   JOIN characters ON characters.id = character_books.character_id;"
 end
